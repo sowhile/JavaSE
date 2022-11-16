d738beb7edeeba364bbb09109269cf13d4bae2ad
sowhile <wdworkmail@qq.com>
2022-11-16 22:58:30 +0800

    hello,world!

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+/**
+ * @author WangDa
+ * @version 1.0
+ * <p>
+ * ${DATE} ${TIME}
+ */
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
