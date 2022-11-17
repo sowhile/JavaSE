9f4c720d6a2c55a07f32f64ebfd9f6705ed86b98
sowhile <wdworkmail@qq.com>
2022-11-17 15:36:55 +0800

    -

---
+++ src/learn/file_/FileWR.java
@@ -0,0 +1,17 @@
+package learn.file_;
+
+import org.junit.jupiter.api.Test;
+
+/**
+ * @author sowhile
+ * @version 1.0
+ * <p>
+ * 2022/11/17 15:36
+ */
+public class FileWR {
+
+    @Test
+    public void read() {
+
+    }
+}
