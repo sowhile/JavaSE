3ff4123b2ca5362bfba555ecefd1da2fdd6ceb6c
sowhile <wdworkmail@qq.com>
2022-11-18 21:19:50 +0800

    11.17

--- src/learn/file_/properties_.java
+++ src/learn/file_/properties_.java
@@ -2,7 +2,6 @@ package learn.file_;
 
 import org.junit.jupiter.api.Test;
 
-import java.io.FileInputStream;
 import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
@@ -18,7 +17,7 @@ public class properties_ {
     @Test
     public void read() throws IOException {
         Properties properties = new Properties();
-        properties.load(new FileReader("c:\\io\\test.properties"));
+        properties.load(new FileReader("src/learn/file_/test.properties"));
         System.out.println(properties.getProperty("ip"));
         properties.list(System.out);
     }
@@ -26,8 +25,10 @@ public class properties_ {
     @Test
     public void write() throws IOException {
         Properties properties = new Properties();
-        properties.load(new FileReader("c:\\io\\test.properties"));
-        properties.setProperty("name", "qwqw");
-        properties.store(new FileWriter("c:\\io\\test.properties"), "hello");
+        properties.load(new FileReader("src/learn/file_/test.properties"));
+        properties.setProperty("name", "xiaohong");
+        properties.setProperty("age", "100");
+        properties.setProperty("ip", "192.168.1.1");
+        properties.store(new FileWriter("src/learn/file_/test.properties"), "hello,world!");
     }
 }
